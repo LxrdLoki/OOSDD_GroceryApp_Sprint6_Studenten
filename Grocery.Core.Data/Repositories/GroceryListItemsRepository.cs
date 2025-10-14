a270f7adaffa069af1abe32c167604ce075cfcf6
LxrdLoki <melvinVD69@outlook.com>
2025-10-14 19:15:26 +0200

    Initialized the database functionality for usecase 18, tested it by making the get all function

--- Grocery.Core.Data/Repositories/GroceryListItemsRepository.cs
+++ Grocery.Core.Data/Repositories/GroceryListItemsRepository.cs
@@ -1,25 +1,53 @@
 ﻿using Grocery.Core.Interfaces.Repositories;
 using Grocery.Core.Models;
+using Microsoft.Data.Sqlite;
 
 namespace Grocery.Core.Data.Repositories
 {
-    public class GroceryListItemsRepository : IGroceryListItemsRepository
+    public class GroceryListItemsRepository : DatabaseConnection, IGroceryListItemsRepository
     {
-        private readonly List<GroceryListItem> groceryListItems;
+        private readonly List<GroceryListItem> groceryListItems = new();
 
         public GroceryListItemsRepository()
         {
-            groceryListItems = [
-                new GroceryListItem(1, 1, 1, 3),
-                new GroceryListItem(2, 1, 2, 1),
-                new GroceryListItem(3, 1, 3, 4),
-                new GroceryListItem(4, 2, 1, 2),
-                new GroceryListItem(5, 2, 2, 5),
-            ];
+            CreateTable(@"
+                        CREATE TABLE IF NOT EXISTS GroceryListItems (
+                            [Id] INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                            [GroceryListId] INTEGER NOT NULL,
+                            [ProductId] INTEGER NOT NULL,
+                            [Amount] INTEGER NOT NULL
+                        );
+                    ");
+
+            List<string> insertQueries = [@"INSERT OR IGNORE INTO GroceryListItems(GroceryListId, ProductId, Amount) VALUES(1, 1, 3)",
+                                            @"INSERT OR IGNORE INTO GroceryListItems(GroceryListId, ProductId, Amount) VALUES(2, 2, 1)",
+                                            @"INSERT OR IGNORE INTO GroceryListItems(GroceryListId, ProductId, Amount) VALUES(3, 3, 4)",
+                                            @"INSERT OR IGNORE INTO GroceryListItems(GroceryListId, ProductId, Amount) VALUES(4, 1, 2)",
+                                            @"INSERT OR IGNORE INTO GroceryListItems(GroceryListId, ProductId, Amount) VALUES(5, 2, 5)"
+                                           ];
+            InsertMultipleWithTransaction(insertQueries);
+            GetAll();
         }
 
         public List<GroceryListItem> GetAll()
         {
+            groceryListItems.Clear();
+            string selectQuery = "SELECT Id, GroceryListId, ProductId, Amount FROM GroceryListItems";
+            OpenConnection();
+            using (SqliteCommand command = new(selectQuery, Connection))
+            {
+                SqliteDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    int id = reader.GetInt32(0);
+                    int groceryListId = reader.GetInt32(1);
+                    int productId = reader.GetInt32(2);
+                    int amount = reader.GetInt32(3);
+                    groceryListItems.Add(new(id, groceryListId, productId, amount));
+                }
+            }
+            CloseConnection();
             return groceryListItems;
         }
 
